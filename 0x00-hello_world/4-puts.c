89f43e9c6f29f21d49ef324597f3f79f733934f9
callistus406 <callistus406@mail.com>
2022-04-07 12:40:05 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
-/**
- * * main - Write '"Programming is like building a multilingual puzzle'
- * *Return: 0 Always 
- * */
+/*
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
