7441cfdc19d4b82cf71cdd775673b57002a0aa5b
callistus406 <callistus406@mail.com>
2022-04-21 12:43:13 -0700

    fixed

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,10 +1,13 @@
 #include "main.h" 
 
-/** 
- * main - check the code 
- * 
- * Return: Always 0. 
- */ 
+/**
+ * print_rev - reverse string
+ *
+ * @s: String to be reversed
+ *
+ * Return: void
+ */
+
 
 void print_rev(char *s)
 {
