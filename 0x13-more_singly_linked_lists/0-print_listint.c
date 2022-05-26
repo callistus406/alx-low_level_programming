a1765d194a04532ec4d504676f12b46d02ab0fc2
callistus406 <callistus406@mail.com>
2022-05-26 09:33:53 -0700

    commit

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,21 @@
-#include <stdio.h>
 #include "lists.h"
-#include <stdlib.h>
-#include <string.h>
-/**
- * print_list - prints to standard output
- * @h: A node in list_t
- * Return: The number of nodes
- */
 
+/**
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t num_of_nodes = 0;
+	size_t n_nodes = 0;
 
-	while (h != NULL )
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		num_of_nodes++;
+		n_nodes++;
 	}
-	return (num_of_nodes);
+	return (n_nodes);
 }
+
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef SINGLY_LINKED_LIST_H_
-#define SINGLY_LINKED_LIST_H_
+#ifndef _LISTS_
+#define _LISTS_
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -18,11 +18,9 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-int _putchar(char c);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
-listint_t *add_nodeint(listint_t **head, const int *n);
-listint_t *add_nodeint_end(listint_t **head, const int *n);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
 #endif 
