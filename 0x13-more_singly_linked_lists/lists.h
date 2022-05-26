417148f1f09919d687823a191f055b99f0f4b05e
callistus406 <callistus406@mail.com>
2022-05-26 08:39:22 -0700

    done

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -20,8 +20,8 @@ typedef struct listint_s
 } listint_t;
 
 int _putchar(char c);
-size_t print_listint(const list_t *h);
-size_t listint_len(const list_t *h);
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
