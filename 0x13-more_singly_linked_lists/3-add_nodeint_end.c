e675960bc326e5a79bd9cc9d1192f6e9e577623e
callistus406 <callistus406@mail.com>
2022-05-26 10:36:15 -0700

    commit for question three

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -15,7 +15,7 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *present;
 	listint_t *new_node;
 
-	current = *head;
+	present  = *head;
 	while (present && present->next != NULL)
 		present = present->next;
 
