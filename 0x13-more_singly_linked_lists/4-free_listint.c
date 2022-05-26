96abbbc7caf02351e7ade169666cedc4049b7452
callistus406 <callistus406@mail.com>
2022-05-26 10:38:29 -0700

    my fourth commit

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: head of linked list
+ */
+
+void free_listint(listint_t *head)
+{
+	listint_t *current;
+	listint_t *nexti_node;
+
+	current = head;
+
+	while (current != NULL)
+	{
+		next_node = current->next_node;
+		free(current);
+		current = next_node;
+	}
+}
