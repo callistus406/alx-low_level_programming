6246c02e0762525a6502233642b3bbb87b7cd612
callistus406 <callistus406@mail.com>
2022-07-31 10:04:59 -0700

    commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -2,30 +2,29 @@
 
 /**
  * hash_table_create - creates a hash table
- *
- * @size: The size of the hash table (the spaces allocated for nodes)
- * Return: pointer to the hash table on success, else, null
+ * @size: size of the table
+ * Return: hash table on success, NULL on failure.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
 
-	if (size < 1)
+	hash_table_t *hash_table;
+
+	if (size == 0)
 		return (NULL);
 
-	ht = malloc(sizeof(hash_table_t));
+	hash_table = malloc(sizeof(hash_table_t));
 
-	if (ht == NULL)
+	if (hash_table == NULL)
 		return (NULL);
 
-	ht->size = size;
-	ht->array = calloc(size, sizeof(hash_node_t *));
-
-	if (ht->array == NULL)
+	hash_table->size = size;
+	hash_table->array = calloc(size, sizeof(hash_node_t *));
+	if (hash_table->array == NULL)
 	{
-		free(ht);
+		free(hash_table);
 		return (NULL);
 	}
+	return (hash_table);
 
-	return (ht);
 }
